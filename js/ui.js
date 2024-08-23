ec73aa695b2bc4fd7921ba790e8cf584c630d7b4
Matheus Fernandes <matheusfernandes311@hotmail.com>
2024-08-22 22:35:07 -0300

    changing to english

--- js/ui.js
+++ js/ui.js
@@ -2,15 +2,15 @@ import api from "./api.js";
 
 const ui = {
   async renderThoughts() {
-    const thoughtsList = document.getElementById("lista-pensamentos");
+    const thoughtsList = document.getElementById("thoughts-list");
     try {
       const thoughts = await api.getThoughts();
       thoughts.forEach((thought) => {
         thoughtsList.innerHTML += `
-        <li class="li-pensamento" data-id="${thought.id}">
+        <li class="li-thought" data-id="${thought.id}">
          <img src="assets/imagens/aspas-azuis.png" alt="Aspas azuis" class="icone-aspas">
-         <div class="pensamento-conteudo">${thought.conteudo}</div>
-         <div class="pensamento-autoria">${thought.autoria}</div>
+         <div class="thought-content">${thought.conteudo}</div>
+         <div class="thought-autoria">${thought.autoria}</div>
         </li>
         `;
       });
